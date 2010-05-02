388bafd15071e03c3185ac6f249ed99a31bdb6e5
Pascal von Bueren <csr_@me.com>
2010-05-02 13:55:31 +0200

    Restructured

---
+++ autoload/autoload.php
@@ -0,0 +1,9 @@
+<?php 
+return array (
+  'DirectoryIteratorFilter' => './bin/ymcgen-autoloads.php',
+  'ymcDynamicIniSetting' => './classes/ymcdynamicinisetting.php',
+  'ymcDynamicIniSettingType' => './datatypes/ymcdynamicinisetting/ymcdynamicinisettingtype.php',
+  'ymcExtensionLoader' => './classes/ymcextensionloader.php',
+  'ymcExtensionLoaderInfo' => './ezinfo.php',
+);
+?>
