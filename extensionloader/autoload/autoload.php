9a4d097b346c57247c97e9a782c09d814e47f4f1
Pascal von Bueren <csr_@me.com>
2010-05-02 13:55:20 +0200

    Changed

--- extensionloader/autoload/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php 
-return array (
-  'DirectoryIteratorFilter' => './bin/ymcgen-autoloads.php',
-  'ymcDynamicIniSetting' => './classes/ymcdynamicinisetting.php',
-  'ymcDynamicIniSettingType' => './datatypes/ymcdynamicinisetting/ymcdynamicinisettingtype.php',
-  'ymcExtensionLoader' => './classes/ymcextensionloader.php',
-  'ymcExtensionLoaderInfo' => './ezinfo.php',
-);
-?>
